468881bc16a6fc37b4bca88024c4d7b254a15cc0
Maarten van weerdt <34571@ma-web.nl>
2022-11-01 14:41:20 +0100

    bo klaar

---
+++ 02-FilesFolders/maak_mappen.py
@@ -0,0 +1,8 @@
+import os
+bestand = open("klasgenoten.txt", "r")
+tekst_regel = bestand.readline()
+while tekst_regel:
+   print(tekst_regel)
+   tekst_regel = bestand.readline()
+   tekst_regel = tekst_regel.strip()
+   os.mkdir(tekst_regel)
---
+++ 02-FilesFolders/maak_tekstfile.py
@@ -0,0 +1,8 @@
+import io
+bestand = open("test.txt", "w")
+
+# Een tekst naar het bestand schrijven
+bestand.write("Test 123!");  
+
+# Vergeet bestand niet te sluiten!
+bestand.close()
